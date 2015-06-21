9be865b2e82e9ff88c5404eb335f3ebb096cca85
gmp26 <gmp26@cam.ac.uk>
2015-06-21 10:51:21 +0100

    lib version updates. Bug in play noticed

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3211"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [rum "0.2.6"]]
+                 [rum "0.2.7"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.3"]]
